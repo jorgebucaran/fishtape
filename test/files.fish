bd49d05d7a61c37154b49fb2a8a1754ebc81c4fc
Jorge Bucaran <mail@jorgebucaran.com>
2019-02-13 12:11:27 +0900

    test: better test descriptions in file.fish

--- test/files.fish
+++ test/files.fish
@@ -8,8 +8,8 @@ function teardown
     command rm -rf $tmp_dir
 end
 
-@test "a file" (touch $tmp_dir/file) -e $tmp_dir/file
-@test "regular file" (touch $tmp_dir/file) -f $tmp_dir/file
+@test "file exists" (touch $tmp_dir/file) -e $tmp_dir/file
+@test "file is regular" (touch $tmp_dir/file) -f $tmp_dir/file
 @test "file is empty" (touch $tmp_dir/file) ! -s $tmp_dir/file
 @test "file is non-empty" (echo foo > $tmp_dir/file) -s $tmp_dir/file
-@test "a directory" -d $tmp_dir
+@test "file is a directory" -d $tmp_dir
