ba970af2a4cb7fb19f0957fbffca8fd188ef876b
mattmc3 <mattmc3@gmail.com>
2021-01-20 11:19:19 -0500

    Add better support for failed not (!) tests

--- tests/files.fish
+++ tests/files.fish
@@ -5,7 +5,9 @@ set temp (command mktemp -d)
 builtin cd $temp
 
 @test "a directory" -d $temp
+@test "a non-existent directory" ! -d $temp.fake
 @test "a regular file" (command touch file) -f file
+@test "a non-existent regular file" ! -f file.fake
 @test "nothing to see here" -z (read <file)
 
 command rm -rf $temp
