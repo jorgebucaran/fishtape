4385355751a48eac107844a5794e0d5d27e446e4
Jorge Bucaran <mail@jorgebucaran.com>
2021-01-22 22:59:11 +0900

    Don't need two tests here

--- tests/files.fish
+++ tests/files.fish
@@ -5,9 +5,8 @@ set temp (command mktemp -d)
 builtin cd $temp
 
 @test "a directory" -d $temp
-@test "a non-existent directory" ! -d $temp.fake
+@test "not a directory" ! -d $temp/foo/bar
 @test "a regular file" (command touch file) -f file
-@test "a non-existent regular file" ! -f file.fake
 @test "nothing to see here" -z (read <file)
 
 command rm -rf $temp
